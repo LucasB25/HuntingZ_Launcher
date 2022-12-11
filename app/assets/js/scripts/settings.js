d6196e140eacd845f580f9cd67d90a20f7899c1c
LucasB25#7323 <50886682+LucasB25@users.noreply.github.com>
2022-12-11 17:16:22 +0100

    Update settings.js

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -385,15 +385,16 @@ ipcRenderer.on(MSFT_OPCODE.REPLY_LOGIN, (_, ...arguments_) => {
     if (Object.prototype.hasOwnProperty.call(queryMap, "error")) {
       switchView(getCurrentView(), viewOnClose, 500, 500, () => {
         // TODO Dont know what these errors are. Just show them I guess.
+        let error = queryMap.error; // Error might be 'access_denied' ?
+        let errorDesc = queryMap.error_description;
         // This is probably if you messed up the app registration with Azure.
+
         console.log(
           "Error getting authCode, is Azure application registered correctly?"
         );
         console.log(error);
-        console.log(error_description);
-        console.log("Full query map", queryMap);
-        let error = queryMap.error; // Error might be 'access_denied' ?
-        let errorDesc = queryMap.error_description;
+        console.log(errorDesc);
+        console.log("Full query map: ", queryMap);
         setOverlayContent(error, errorDesc, "OK");
         setOverlayHandler(() => {
           toggleOverlay(false);
